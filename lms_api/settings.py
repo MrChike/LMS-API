cc121f6598670222821dc63b91fbee9066729f64
chike <chikeegonu@gmail.com>
2024-11-16 06:26:30 +0100

    Add render to cors allowed origins III

--- lms_api/settings.py
+++ lms_api/settings.py
@@ -155,3 +155,17 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'Authorization',
+    'X-Requested-With',
+    'X-CSRFToken',
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
+    'OPTIONS',
+]
